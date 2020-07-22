80134451afbc0702e545ed0930011a06dcf44183
qwestionmark <qwestionmark90@gmail.com>
2020-07-21 21:23:43 -0400

    Implemented Resolvers.

--- backend_node_graphql/src/index.js
+++ backend_node_graphql/src/index.js
@@ -1,39 +1,53 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`
-
-let links = [{
+const links = [{
   id: 'link-0',
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL'
 }]
 
+let idCount = links.length
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
+    link: (id) => links.find(link => link.id ===`link-${id}`),
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-  }
+  Mutation: {
+    post: (parent, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    },
+    updateLink: (parent, args) => {
+      const { id, url, description } = args;
+      const linkIdx = links.findIndex(link => link.id === `link-${id}`);
+      const link = links[linkIdx];
+      if (!link) return 'No link found with that ID';
+      if (url) link.url = url;
+      if (description) link.description = description;
+      links.splice(linkIdx, 1, link);
+      return link
+    },
+    deleteLink: (parent, args) => {
+      const id = args.id;
+      const linkIdx = links.findIndex(link => link.id === `link-${id}`);
+      const link = links[linkIdx];
+      if (!linkIdx) return 'No link found with that ID';
+      links.splice(linkIdx, 1);
+      return link
+    }
+    
+  },
 }
 
+
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
