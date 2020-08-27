8e70ee3260a51cf5cc09e44aea3b08a6a7aba4e0
Trey <qwestionmark90@gmail.com>
2020-08-26 22:08:08 -0400

    Using sequelize models in message & user resolvers.

--- rwieruch-apollo-server/src/resolvers/message.js
+++ rwieruch-apollo-server/src/resolvers/message.js
@@ -1,53 +1,29 @@
-import { uuidv4 } from 'uuid';
-
 export default {
-    Query: {
-        messages: (parent, args, { models }) => {
-            return Object.values(models.messages);
-        },
-          message: (parent, { id }, { models }) => {
-            return models.messages[id];
-        },
+  Query: {
+    messages: async (parent, args, { models }) => {
+      return await models.Message.findAll();
     },
-    Mutation: {
-        createMessage: (parent, { text }, { me, models }) => {
-            const id = uuidv4();
-            const message = {
-              id,
-              text,
-              userId: me.id,
-            };
-       
-            models.messages[id] = message;
-            models.users[me.id].messageIds.push(id);
-       
-            return message;
-          },
-    
-        deleteMessage: (parent, { id }, { models }) => {
-            const { [id]: message, ...otherMessages } = models.messages;
-        
-            if (!message) {
-                return false;
-            }
-        
-            models.messages = otherMessages;
-        
-            return true;
-        },
-    
-        updateMessage: (_, { id, text }, { models }) => {
-            const { [id]: message } = models.messages;
-    
-            if (!message) return `Message with id: ${id} not found`;
-            message.text = text
-            models.messages[id] = message;
-            return message;
-        },
+    message: async (parent, { id }, { models }) => {
+      return await models.Message.findByPk(id);
     },
-    Message: {
-        user: (message, args, { models }) => {
-          return models.users[message.userId];
-        },
-      },
-}
+  },
+ 
+  Mutation: {
+    createMessage: async (parent, { text }, { me, models }) => {
+      return await models.Message.create({
+        text,
+        userId: me.id,
+      });
+    },
+ 
+    deleteMessage: async (parent, { id }, { models }) => {
+      return await models.Message.destroy({ where: { id } });
+    },
+  },
+ 
+  Message: {
+    user: async (message, args, { models }) => {
+      return await models.User.findByPk(message.userId);
+    },
+  },
+};
--- rwieruch-apollo-server/src/resolvers/user.js
+++ rwieruch-apollo-server/src/resolvers/user.js
@@ -1,21 +1,23 @@
-
 export default {
-    Query: {
-        users: (parent, args, { models }) => {
-          return Object.values(models.users);
-        },
-        user: (parent, { id }, { models }) => {
-          return models.users[id];
-        },
-        me: (parent, args, { me }) => {
-          return me;
-        },
+  Query: {
+    users: async (parent, args, { models }) => {
+      return await models.User.findAll();
     },
-    User: {
-        messages: (user, args, { models }) => {
-            return Object.values(models.messages).filter(
-            message => message.userId === user.id,
-            );
-        },
+    user: async (parent, { id }, { models }) => {
+      return await models.User.findByPk(id);
     },
-}
+    me: async (parent, args, { models, me }) => {
+      return await models.User.findByPk(me.id);
+    },
+  },
+ 
+  User: {
+    messages: async (user, args, { models }) => {
+      return await models.Message.findAll({
+        where: {
+          userId: user.id,
+        },
+      });
+    },
+  },
+};
